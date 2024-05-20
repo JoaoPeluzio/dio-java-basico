3097e8a3a7666f54a69dd409a92820583b1230e2
JoaoPeluzio <jvpeluzio@gmail.com>
2024-05-20 16:33:04 -0300

    atualizando dados projeto ContaBanco

--- primeiro-projeto-java/src/ContaBanco.java
+++ primeiro-projeto-java/src/ContaBanco.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class ContaTerminal {
+public class ContaBanco {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US); 
