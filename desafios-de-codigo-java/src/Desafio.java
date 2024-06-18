aa10d270e0507eaa9d840c6670879a926e2468d9
JoaoPeluzio <jvpeluzio@gmail.com>
2024-06-18 12:11:28 -0300

    desafio de codigo java

---
+++ desafios-de-codigo-java/src/Desafio.java
@@ -0,0 +1,34 @@
+//Para ler e escrever dados em Java, na DIO padronizam desta forma:
+// - new Scanner(Systen.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// - System.out.println: imprime um texto de Saída (output) e pulando uma linha.
+
+
+import java.util.Scanner;
+
+public class Desafio {
+    
+
+    public static void main(String[] args) {
+    
+        Scanner leitorDeEntradas = new Scanner(System.in);
+        
+        float valorSalario = leitorDeEntradas.nextFloat();
+        float valorBeneficios = leitorDeEntradas.nextFloat();
+
+        float valorImposto = 0;
+
+        if(valorSalario >= 0 && valorSalario <= 1100) {
+            valorImposto = 0.05F * valorSalario;
+        } else if (valorSalario >= 1100.01 && valorSalario <= 2500.00) {
+            valorImposto = 0.10F * valorSalario;
+        } else {
+            valorImposto = 0.15F * valorSalario;
+        }
+
+        float saida = valorSalario - valorImposto + valorBeneficios;
+        System.out.printf(String.format("R$ %.2f\n", saida));
+    
+    }
+
+
+}
